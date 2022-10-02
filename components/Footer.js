c15ef79385f151b19e59b7afc9303fd39527b6c5
Adam <adamassalimy@gmail.com>
2022-10-02 13:30:37 -0700

    commit initial files

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Footer(){
+    return(
+       
+        <div className="icons">
+            <a href="https://github.com/XAdam3" target="_blank" className="icon" >
+                <i  className="fa-brands fa-github"></i>
+            </a>
+                
+            <a href="https://www.behance.net/AdamAssalimy" target="_blank" className="icon" >
+                <i className="fa-brands fa-square-behance"></i>
+            </a>
+            
+            <a href="https://dev-adam.netlify.app" target="_blank" className="icon" >
+                <i  className="fa-solid fa-earth-americas"></i>
+            </a>
+        </div>
+      
+    )
+}
---
+++ components/Interests.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Interests(){
+    return (
+        <div className="content">
+        <div className="interests">
+            <h3 className="interests-title">Interests</h3>
+            <p className="interests-text">Art. Sketching. Coding. Graphic Design. Soccer </p>
+        </div>
+        </div>
+    )
+}
+
